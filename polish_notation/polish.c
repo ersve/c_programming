a2a4f85091e1dea0f44dcd79cc13669d22d3b741
Erik <e.svensson9309@gmail.com>
2020-03-28 22:08:55 +0100

    Added memory handling for the stack, also implemented a function to clear the values of the stack without freeing the memory so that it may be re-used.

--- polish_notation/polish.c
+++ polish_notation/polish.c
@@ -5,248 +5,144 @@
 
 
 #define MAX_SYMBOLS 2048
-#define MAX_INT 10
+#define MAX_STRING 4
 #define TRUE 1
 #define FALSE 0
 #define NONE '\0'
 
-int TYPE_INT = 0;
-int TYPE_STRING = 1;
-int TYPE_BOOLEAN = 2;
-int TYPE_PERSON = 3;
 
 
-// Linked node
-struct Node {
-	char data;
-	struct Node *next;
-	struct Node *prev;
+// A struct to keep the contents and meta information about the stack
+struct LIFO {
+	char **array;
+	int index;
+	unsigned size;
 };
-typedef struct Node Node;
 
-// struct to hold properties of the list
-struct List {
-	unsigned length;
-	Node *head;
-	Node *tail;
-};
-typedef struct List List;
+typedef struct LIFO Stack;
 
-void init(List *list) {
-	list->length = 0;
-	list->head = NULL;
-	list->tail = NULL;
-}
-
-int isempty(List *list){
-	return (list->tail == NULL);
-}
-
-void clean(List *list){
-	Node *removed_node; 
-	while(!isempty(list)){		
-		// appoint first node to temporary pointer
-		removed_node = list->head;
-		// if we are at the final node move tail poiner to the node pointed out by the first
-		if (list->head == list->tail){
-			list->tail = list->tail->next; //should be NULL
-		}
-		// move head poiner to the node pointed out by the first
-		list->head = list->head->next;
-		// decrease list length
-		list->length--;
-		// free memory allocated by node
-		free(removed_node);		
-	}
-	// remove pointer address	
-	removed_node = NULL;
-}
-
-
-void push(List *list, char data ) {
-	Node *new_node;
+// Function to copy the contents from one string to another
+char *copy_string(char *target, char *source){
 	
-	//Create a new node and populate it with data
-	new_node = (Node*)malloc(sizeof(Node));
-	if(new_node == NULL) {
-		fprintf(stderr,"ERROR:Unable to allocate memory");
-		clean(list);
-		exit(1);
-	}
-	else {
-		new_node->data = data;
-		new_node->next = NULL;
+	// Return if no memory has been allocated
+	if ( target == NULL || source == NULL) {
+		return NULL;		
 	}
 	
+	// Pointer to the beginning of the target
+	char *ptr = target;
 	
-	if (!isempty(list)){ 
-		//Insert the node in the list
-		list->tail->next = new_node;
-		list->tail=new_node;
-	}
-	else { 
-		// first element of the list
-		list->head = new_node;
-		list->tail = new_node;
-	}
-	list->length++;
-	new_node = NULL;
-			
-}
-
-void pop(List *list, char *data) {
-	// create temporary node pointer
-	if (!isempty(list)){
-		Node *removed_node; 
-		// retreive data
-		*data = list->head->data;
-		// appoint first node to temporary pointer
-		removed_node = list->head;
-		// if we are at the final node move tail poiner to the node pointed out by the first
-		if (list->head == list->tail){
-			list->tail = list->tail->next; // should be NULL
-		}
-		// move head poiner to the node pointed out by the first
-		list->head = list->head->next;
-		// decrease list length
-		list->length--;
-		// free memory allocated by node
-		free(removed_node);
-		// remove pointer address
-		removed_node = NULL;
-		
-	}
-	else {
-		*data = NONE;
-	}
-}
-
-void peak(List *list, char *data){
-	if (!isempty(list)){
-		*data =  list->head->data;
-	}
-	else {
-		*data = NONE;
-	}
-}
-
-char *toArray(List *list){
-	int i;
-	char *ptr;
-	Node *prnt;
+	while (( *target++ = *source++) != NONE);
 	
-	ptr = (char*)malloc((list->length+1)*sizeof(char));	
-	if(ptr == NULL) {
-		fprintf(stderr,"ERROR:Unable to allocate memory");
-		clean(list);
-		exit(1);
-	}
+	// End the string with NUL
+	*target = '\0';
 	
-	prnt = list->head;
-	for(i = 0; i<list->length; i++){
-		*(ptr+i) = prnt->data;
-		prnt = prnt->next;
-	}
-	*(ptr+i) = NONE;
+	// Rewind target to original element
+	target = ptr;
+	
 	return ptr;
 }
-
-void printL(List *list){
-	char *ptr = toArray(list);
-	if(list->length >0){
-		printf("%s\n",ptr);
+// Function to initialize the stack
+Stack *Stack_create(unsigned size) {
+	
+	Stack *new_stack = (Stack *)malloc(sizeof(Stack));
+	
+	if(new_stack == NULL){
+		perror("Unable to allocate memory: new_stack \n");
+		exit(1);
 	}
-	free(ptr);
+	
+	new_stack->array = (char **)malloc(size * sizeof(char*));
+	new_stack->size = size;
+	new_stack->index = -1;
+	
+	if(new_stack->array == NULL){
+		perror("Unable to allocate memory: array \n");
+		exit(1);
+	}
+	
+	return new_stack;
+	
+}
+// Chek if stack is empty
+int Stack_is_empty(Stack *stack) {
+	return (stack->index == -1);
+}
+// Check if stack is full
+int Stack_is_full(Stack *stack) {
+	return (stack->index == stack->size-1);
+}
+// Function to check the top value on the stacl
+char *Stack_peek(Stack *stack) {
+	if (!Stack_is_empty(stack)) {
+		return stack->array[stack->index];
+	}
+	return NONE;
+}
+// Function to remove the latest appended value from the stack
+char *Stack_pop(Stack *stack) {
+	if (!Stack_is_empty(stack)) {
+		return stack->array[stack->index--];
+	}
+	return NONE;
+}
+// Function to push a string into the stack
+int Stack_push(Stack *stack, char *string) {
+	
+	if (!Stack_is_full(stack)) {
+		// Move to next index
+		stack->index++;
+		
+		// if element not initiated
+		if (stack->array[stack->index] == NULL) {
+			stack->array[stack->index] = malloc(MAX_STRING*sizeof(char));
+		}
+		// return operation outcome
+		return copy_string(stack->array[stack->index],string) ? TRUE : FALSE;		
+		
+	}
+	return 0;
 }
 
-int isNumber(char data){
-	return ( data >= 40 && data <= 57);
-	}
-	
-int isOperator(char data){
-	switch(data){
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-			return 1;
-			break;
-		default:
-			return 0;
-		}
-	}
-	
-int parse_int(List *list){
-	char data;
+// Function to free all the memory allocated by the stack
+void Stack_free(Stack *stack) {
 	int i = 0;
-	char intArr[3];
-	char *pIntArr = &intArr[0];
-	peak(list,&data);
-	while(isNumber(data)){
-		pop(list,(pIntArr+i));
-		peak(list,&data);
-		i++;
+	// Free each string in array
+	for ( i=0; i<stack->size; i++){
+		free(stack->array[i]);
 	}
-	*(pIntArr+i) = NONE;
-	return atoi(pIntArr);
+	// Free pointer to strings
+	free((*stack).array);
+	
+	// Free struct
+	free(stack);
+
 }
+// function to reset the stack without freeing memory
+void Stack_clear(Stack *stack){
+	int i = 0;
 	
-	
-
-int main(){
-	
-	List *input, *output;
-	char data;
-	int d;
-	input = malloc(sizeof(List));
-	output = malloc(sizeof(List));
-	init(input);
-	init(output);
-	/*
-	while(TRUE){
-		
-		if(feof(stdin)){break;}
-		
-		// Read line into list
-		while(TRUE){
-			data = getchar();
-			if (data == EOF || data == '\n'){
-				break;
-			}
-			else {
-				push(input,data);
-			}
-
-		}
-		
-		// Parse and simplify
-		
-		
-		// print list onto stdout
+	//Start all of the allocated strings with NUL
+	for ( i=0; stack->array[i] != NULL && i<stack->size; i++){
+		stack->array[i][0] = NONE;
 	}
-	free(input);
-	*/
-	
-	push(input,'9');
-	push(input,'-');
-	push(input,'2');
-	d = parse_int(input);
-	printf("%d\n",d);
-	
-} // end of main
+	stack->index = -1;
+}
+
+read_value
 	
 	
+int main(){
+	Stack *stack = Stack_create(MAX_SYMBOLS);
+	printf("Push is successful:%d\n",Stack_push(stack,"-9"));
+	printf("Push is successful:%d\n",Stack_push(stack,"-10"));
+	printf("%s\n",Stack_pop(stack));
+	printf("%s\n",Stack_pop(stack));
+	Stack_free(stack);
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+}
+		
 	
 	
 	
